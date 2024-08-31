edaf66cd72e6955eca71c9de421c5d4b53f38605
玛卡巴卡 <2781463390@qq.com>
2024-08-31 10:12:09 +0800

    文章列表渲染🟢

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,37 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), preact()]
-});
+  integrations: [mdx(), sitemap(), preact()],
+  markdown: {
+    shikiConfig: {
+      // 选择 Shiki 内置的主题（或添加你自己的主题）
+      // https://shiki.style/themes
+      theme: 'dracula',
+      // 另外，也提供了多种主题
+      // 查看下面关于使用亮/暗双主题的的说明
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark'
+      },
+      // 禁用默认配色
+      // https://shiki.style/guide/dual-themes#without-default-color
+      // (添加于 v4.12.0)
+      defaultColor: false,
+      // 添加自定义语言
+      // 注意：Shiki 内置了无数语言，包括 .astro！
+      // https://shiki.style/languages
+      langs: [],
+      // 启用自动换行，以防止水平滚动
+      wrap: true,
+      // 添加自定义转换器：https://shiki.style/guide/transformers
+      // 查找常用转换器：https://shiki.style/packages/transformers
+      transformers: []
+    }
+  }
+})
